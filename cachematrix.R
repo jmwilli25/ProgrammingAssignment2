062f1223b50396e40ffd7030c9af049d3c55ec5e
Jeremy Williams <jmwilli25@gmail.com>
2015-01-25 23:41:27 -0500

    Calculates inverse matrix and puts the result in cache. Returns the value from the cache every time after that when called rather than recalculating.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# x <- matrix(c(2,6,1,9),2,2)
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseMtx <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseMtx <<- NULL
+  } 
+  getMatrix <- function() x
+  getInverseMtx <- function() inverseMtx
+  setInverseMtx <- function(y) inverseMtx <<- y
+  list(set = set, getMatrix = getMatrix, getInverseMtx = getInverseMtx, setInverseMtx = setInverseMtx)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(z, ...) {
+  if(!is.null(z$getInverseMtx())) { # Returns inverse of matrix from cache if not null
+    message("Getting cached data")
+    inverseMtx <- z$getInverseMtx()
+    return(inverseMtx)
+  } else { # If inverse of matrix is not in cache this calculates, sets the inverse into the cache, then calls the inverse from the cache
+    matrix <- z$getMatrix()
+    inverseMtx <- solve(matrix)
+    z$setInverseMtx(inverseMtx)
+    return(z$getInverseMtx())
+  }
 }
